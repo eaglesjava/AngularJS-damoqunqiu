b2afaf6262b17c880fa5300f9d92bfdaef82c7f0
scott <1028282161@qq.com>
2015-07-08 13:06:04 +0800

    创建一个新的控制器

---
+++ w3cschool-angularjs/angularjs-05-xingde-controller.js
@@ -0,0 +1,14 @@
+function namesController($scope) {
+
+	$scope.names = [{
+		name: 'Jani',
+		country: 'Norway'
+	}, {
+		name: 'Hege',
+		country: 'Sweden'
+	}, {
+		name: 'Kai',
+		country: 'Denmark'
+	}];
+
+}
