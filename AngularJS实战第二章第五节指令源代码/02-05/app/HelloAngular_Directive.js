9b92ade48dbab799003436623f3286b8d0755b68
雪地伤孤 <1028282161@qq.com>
2015-07-13 23:37:33 +0800

    A:attribute-属性 E：element--元素 M：comment-注释 C：class-样式类

--- AngularJS实战第二章第五节指令源代码/02-05/app/HelloAngular_Directive.js
+++ AngularJS实战第二章第五节指令源代码/02-05/app/HelloAngular_Directive.js
@@ -1,6 +1,7 @@
 var myModule = angular.module("MyModule", []);
 myModule.directive("hello", function() {
     return {
+    	/*A:attribute-属性 E：element--元素 M：comment-注释 C：class-样式类*/
         restrict: 'AEMC',
         template: '<div>Hi everyone!</div>',
         replace: true
